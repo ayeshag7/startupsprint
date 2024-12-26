733562525bd3fc1d6f9f63cf905912f29f04c8b6
shazamch <shazamrazzaq1234@gmail.com>
2024-12-26 15:45:27 +0500

    Fix merge conflict in userRoutes.js

--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-// routes/userRoutes.js
-=======
 // routes/userRoutes.js
 const express = require('express');
 const userController = require('../controllers/userController');
@@ -13,4 +10,3 @@ router.put('/:id', userController.updateUser);
 router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
->>>>>>> 50a3b3fe5d2982f46a8b02163c5353d9808888e8
