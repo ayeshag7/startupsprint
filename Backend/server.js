520004cd096633b63512e61fdea27196f591b795
shazamch <shazamrazzaq1234@gmail.com>
2024-12-26 15:15:19 +0500

    Implement user management features with CRUD operations and add middleware for logging and response handling & CORS

--- Backend/server.js
+++ Backend/server.js
@@ -1,9 +1,15 @@
 require('dotenv').config();
 const express = require('express');
 const dbConnect = require('./src/utils/dbConnect.js');
+const responseHandler = require('./src/middlewares/responseHandler.js');
+const cors = require('./src/middlewares/cors.js');
+const logger = require('./src/middlewares/logger.js');
 const app = express();
 
 app.use(express.json());
+app.use(cors);
+app.use(logger);
+app.use(responseHandler);
 
 
 const port = process.env.PORT || 3000;
