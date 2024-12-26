25ea5b513f1e0fb81592c6da6fc363b2416dc625
shazamch <shazamrazzaq1234@gmail.com>
2024-12-26 16:31:13 +0500

    Added comment, post, and postlike models with Mongoose schemas

---
+++ Backend/src/models/postModel.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const postSchema = mongoose.Schema(
+  {
+    userID: {
+      type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    posttext: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    postphoto: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    privacy: {
+      type: String,
+      default: 'Everyone',
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+module.exports = Post;
