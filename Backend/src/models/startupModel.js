954f1ae8bb1b30f16123ebc84db199cb31351c1d
shazamch <shazamrazzaq1234@gmail.com>
2024-12-26 16:47:04 +0500

    Added investment and startup models with Mongoose schemas

---
+++ Backend/src/models/startupModel.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+
+const linkSchema = mongoose.Schema({
+    facebook: { type: String, default: '' },
+    instagram: { type: String, default: '' },
+    linkedin: { type: String, default: '' },
+    twitter: { type: String, default: '' },
+    website: { type: String, default: '' },
+    morelinks: { type: [String], default: [] },
+  });
+
+const startupSchema = mongoose.Schema(
+  {
+    userID: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref:"User"
+    },
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    description: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    profilephoto: {
+      type: String,
+      default: '',
+      required: false,
+    },
+    funds: {
+      type: String,
+      required: true,
+    },
+    links: {
+        type: linkSchema,
+        default: {},
+        required: false,
+      },
+    status: {
+      type: String,
+      default: 'Active',
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const Startup = mongoose.model('Startup', startupSchema);
+
+module.exports = Startup;
